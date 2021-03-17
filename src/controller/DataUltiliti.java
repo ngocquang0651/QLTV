95e4690bf009ba351fecce7a3d3506be92281712
Dia <baosonheo145@gmail.com>
2021-03-17 11:42:52 +0700

    17/3 update

--- src/controller/DataUltiliti.java
+++ src/controller/DataUltiliti.java
@@ -3,5 +3,43 @@
  */
 package controller;
 
+import model.Book;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class DataUltiliti {
+    private FileWriter fileWriter;
+    private BufferedWriter bufferedWriter;
+    private PrintWriter printWriter;
+    private Scanner scanner;
+void openFileToWrite (String fileName ){
+    try {
+        fileWriter = new FileWriter(fileName,true);
+        bufferedWriter = new BufferedWriter(fileWriter);
+        printWriter = new PrintWriter(bufferedWriter);
+    }catch (IOException e){
+        e.printStackTrace();
+    }
+
+}
+void writeBookToFile (Book book, String fileName){
+    openFileToWrite(fileName);
+    printWriter.println(book.getBookID()+"|"+book.getBookName()+"|"+book.getAuthor()+"|"+book.getSpecialization()
+    +"|"+book.getPublishYear()+"|"+book.getQuantity());
+    closeFileAfterWrite(fileName);
+}
+
+void closeFileAfterWrite (String fileName){
+    try {
+        printWriter.close();
+        bufferedWriter.close();
+        fileWriter.close();
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+}
 }
--- src/view/View.java
+++ src/view/View.java
@@ -4,6 +4,8 @@
 
 package view;
 
-
 public class View {
+    public static void main(String[] args) {
+
+    }
 }
